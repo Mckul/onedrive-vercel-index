7983ab78cf0b19c950ca47d2399d72f29a17c0db
Mckul <74099038+Mckul@users.noreply.github.com>
2022-04-10 03:44:16 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,12 @@ import SwitchLayout from '../components/SwitchLayout'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-grey dark:bg-grey-900">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-white dark:bg-gray-900">
       <Head>
         <title>{siteConfig.title}</title>
       </Head>
 
-      <main className="flex w-full flex-1 flex-col bg-animage dark:bg-grey-800">
+      <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800">
         <Navbar />
         <div className="mx-auto w-full max-w-5xl p-4">
           <nav className="mb-4 flex items-center justify-between pl-1">
